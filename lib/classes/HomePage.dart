a845825304745c382ee2ea8a58a8ca62660ae21c
Nuaym Syed <scienceboy75@gmail.com>
2024-08-08 17:21:53 -0700

    Remove jsastem (Code no longer used)

--- lib/classes/HomePage.dart
+++ lib/classes/HomePage.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
--- lib/services/stemmer.dart
+++ lib/services/stemmer.dart
@@ -28,7 +28,7 @@ class Stemmer{
 
   static String wordStemmer(var word){
     //Remove diacritics
-    word = removeAll(word, RegExp(r'\p{M}', caseSensitive: false, unicode: true), []);
+    word = removeAllHarakat(word);
     //Remove prefixes and suffixes
     if (word.length >= 6) { word = removeAll( word, RegExp(r'^(كال|بال|ولل|وال)(.*)', unicode: true),[2]); }
     if (word.length >= 5) { word = removeAll(word, RegExp(r'^(ال|لل)(.*)', unicode: true),[2]); }
@@ -37,15 +37,13 @@ class Stemmer{
     
     // Remove initial waw (if found)
     if (word.length >= 4) { word = removeAll(word, RegExp(r'^وو', unicode: true),[]); }
-    //Normalize initial hamza to bare alif
-    //if (word.length >= 4) { word = removeAll(word, RegExp(r'^[آأإ]', unicode: true), 'ا'); }
 
     if(word.length <= 3) return word;
 
     //Process advanced patterns (4 letter roots)
     if(word.length == 6){
-      word = removeAll( word, RegExp(r'^[ام]ست(...)', unicode: true),[1]);// مستفعل - استفعل 
-      word = removeAll( word, RegExp(r'^[ام]ست(...)', unicode: true),[1]);// مستفعل - استفعل
+      word = removeAll( word, RegExp(r'^[ام]ست(...)', unicode: true),[1]);//  استفعل 
+      word = removeAll( word, RegExp(r'^[ام]ست(...)', unicode: true),[1]);// استفعل
       word = removeAll( word, RegExp(r'^[تم](.)ا(.)ي(.)', unicode: true),[1,2,3]); // تفاعيل - مفاعيل
       word = removeAll( word, RegExp(r'^م(..)ا(.)ة', unicode: true),[1,2]);// مفعالة
       word = removeAll( word, RegExp(r'^ا(.)[تط](.)ا(.)', unicode: true),[1,2,3]);// افتعال 
@@ -64,32 +62,32 @@ class Stemmer{
 
     if(word.length == 5){
       word = removeAll(word, RegExp(r'^ا(.)[اتط](.)(.)', unicode: true),[1,2,3]);  //   افتعل   -  افاعل
-      word = removeAll(word, RegExp(r'^م(.)(.)[يوا](.)', unicode: true),[1,2,3]); //   مفعول  -   مفعال  -   مفعيل
-      word = removeAll(word, RegExp(r'^[اتم](.)(.)(.)ة', unicode: true),[1,2,3]); //   مفعلة  -    تفعلة   -  افعلة
-      word = removeAll(word, RegExp(r'^[يتم](.)[تط](.)(.)', unicode: true),[1,2,3]); //   مفتعل  -    يفتعل   -  تفتعل
-      word = removeAll(word, RegExp(r'^[تم](.)ا(.)(.)', unicode: true),[1,2,3]);  //   مفاعل  -  تفاعل
-      word = removeAll(word, RegExp(r'^(.)(.)[وا](.)ة', unicode: true),[1,2,3]);  //   فعولة  -   فعالة
-      word = removeAll(word, RegExp(r'^[ما]ن(.)(.)(.)', unicode: true),[1,2,3]);  //   انفعل   -   منفعل
-      word = removeAll(word, RegExp(r'^ا(.)(.)ا(.)', unicode: true),[1,2,3]);     //    افعال
-      word = removeAll(word, RegExp(r'^(.)(.)(.)ان', unicode: true),[1,2,3]);     //    فعلان
-      word = removeAll(word, RegExp(r'^ت(.)(.)ي(.)', unicode: true),[1,2,3]);     //    تفعيل
-      word = removeAll(word, RegExp(r'^(.)ا(.)و(.)', unicode: true),[1,2,3]);     //    فاعول
-      word = removeAll(word, RegExp(r'^(.)وا(.)(.)', unicode: true),[1,2,3]);     //    فواعل
-      word = removeAll(word, RegExp(r'^(.)(.)ائ(.)', unicode: true),[1,2,3]);     //    فعائل
-      word = removeAll(word, RegExp(r'^(.)ا(.)(.)ة', unicode: true),[1,2,3]);     //    فاعلة
-      word = removeAll(word, RegExp(r'^(.)(.)ا(.)ي', unicode: true),[1,2,3]);     //    فعالي
+      word = removeAll(word, RegExp(r'^م(.)(.)[يوا](.)', unicode: true),[1,2,3]); //   مفعول - مفعال -  مفعيل
+      word = removeAll(word, RegExp(r'^[اتم](.)(.)(.)ة', unicode: true),[1,2,3]); //   مفعلة-  تفعلة - افعلة
+      word = removeAll(word, RegExp(r'^[يتم](.)[تط](.)(.)', unicode: true),[1,2,3]); //   مفتعل -  يفتعل - تفتعل
+      word = removeAll(word, RegExp(r'^[تم](.)ا(.)(.)', unicode: true),[1,2,3]);  //   مفاعل - تفاعل
+      word = removeAll(word, RegExp(r'^(.)(.)[وا](.)ة', unicode: true),[1,2,3]);  //   فعولة -  فعالة
+      word = removeAll(word, RegExp(r'^[ما]ن(.)(.)(.)', unicode: true),[1,2,3]);  //   انفعل  -  منفعل
+      word = removeAll(word, RegExp(r'^ا(.)(.)ا(.)', unicode: true),[1,2,3]);     //  افعال
+      word = removeAll(word, RegExp(r'^(.)(.)(.)ان', unicode: true),[1,2,3]);     //  فعلان
+      word = removeAll(word, RegExp(r'^ت(.)(.)ي(.)', unicode: true),[1,2,3]);     //  تفعيل
+      word = removeAll(word, RegExp(r'^(.)ا(.)و(.)', unicode: true),[1,2,3]);     //  فاعول
+      word = removeAll(word, RegExp(r'^(.)وا(.)(.)', unicode: true),[1,2,3]);     //  فواعل
+      word = removeAll(word, RegExp(r'^(.)(.)ائ(.)', unicode: true),[1,2,3]);     //  فعائل
+      word = removeAll(word, RegExp(r'^(.)ا(.)(.)ة', unicode: true),[1,2,3]);     //  فاعلة
+      word = removeAll(word, RegExp(r'^(.)(.)ا(.)ي', unicode: true),[1,2,3]);     //  فعالي
       if(word.length == 3) return word;
-      word = removeAll( word, RegExp(r'^[اتم]', unicode: true),[]);//    تفعلل - افعلل - مفعلل
+      word = removeAll( word, RegExp(r'^[اتم]', unicode: true),[]);               // تفعلل - افعلل- مفعلل
       word = removeAll( word, RegExp(r'[ةهيكتان]', unicode: true),[]);
-      word = removeAll( word, RegExp(r'^(..)ا(..)', unicode: true),[1,2]); //    فعالل
-      word = removeAll( word, RegExp(r'^ا(...)ا(.)', unicode: true),[1,2]); // فعلال
+      word = removeAll( word, RegExp(r'^(..)ا(..)', unicode: true),[1,2]);        //  فعالل
+      word = removeAll( word, RegExp(r'^ا(...)ا(.)', unicode: true),[1,2]);       // فعلال
       word = removeAll( word, RegExp(r'^[لبفسويتنامك]', unicode: true),[]);
     }
 
     if(word.length == 4){
       word = removeAll( word, RegExp(r'^م(.)(.)(.)', unicode: true),[1,2,3]);     // مفعل
       word = removeAll( word, RegExp(r'^(.)ا(.)(.)', unicode: true),[1,2,3]);     // فاعل
-      word = removeAll( word, RegExp(r'^(.)(.)[يوا](.)', unicode: true),[1,2,3]); // فعال   -   فعول    - فعيل
+      word = removeAll( word, RegExp(r'^(.)(.)[يوا](.)', unicode: true),[1,2,3]); // فعال -  فعول - فعيل
       word = removeAll( word, RegExp(r'^(.)(.)(.)ة', unicode: true),[1,2,3]);     // فعلة
       if (word.length == 3 ) { return word; }
 
